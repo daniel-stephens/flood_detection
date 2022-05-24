faae0b1a8470555109486883fafa80f49290079d
daniel-stephens <danystevo@gmail.com>
2022-05-24 11:31:27 -0400

    ROI_to_Video_Completed

--- get_roi.py
+++ get_roi.py
@@ -9,19 +9,23 @@ def roi_crop(image_path):
 
     im = cv2.imread(images[0])
     roi = cv2.selectROI(im)
-    print(roi[0])
-    return roi
+    
+    n = 0
+    for image in images:
+        im = cv2.imread(image)
+        cropped_image = im[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
+        name = '.\\roi\\cropped'+str(n)+".png"
+        cv2.imwrite(name, cropped_image)
+        print(str(n)+" number of images completed")
+        n+=1
+
+
 
 
 
 if __name__ == '__main__':
 
+    folder = ".\\total_frames\\"
 
-
-    folder= ".\\total_frames\\"
     roi_crop(folder)
-#images = sorted(glob.glob(folder+'*.png'), key=len)
-# for i in images:
-#     print(i)
-
 
---
+++ make_video.py
@@ -0,0 +1,30 @@
+from turtle import width
+import cv2
+import os
+import glob
+
+def frames_to_video(frames_path, output_path):
+    images = sorted(glob.glob(f'{os.getcwd()}{frames_path}'), key=len )
+    
+
+    # This block of code gets the dimensions of the image and appends the images to a list
+    frame_array =[]
+    for i in range(len(images)):
+        image  = images[i]
+        img = cv2.imread(image)
+        height, width, layers = img.shape
+        size = (width, height)
+        frame_array.append(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+
+    # This block of code takes the list of images and the size, and makes a video from it
+    out = cv2.VideoWriter(output_path, -1, 30, size) 
+
+    for i in range(len(frame_array)):
+        out.write(frame_array[i])
+    out.release()
+
+if __name__=="__main__":
+    frames_path = ".\\roi\\*.png"
+    output = ".\\videos\\roi_video.mp4"
+
+    frames_to_video(frames_path, output)
